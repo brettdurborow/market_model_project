35a5d3963c6ad5389bcb1b42be58a35d45eb8dd0
patrickshami <patrickshami@e7fa7c23-505d-4fac-8005-645d41c176d8>
2018-03-16 05:09:06 +0000

    - Write Molecule Point Share and Patient Share to output sheet

--- formatTab_Outputs.m
+++ formatTab_Outputs.m
@@ -3,9 +3,9 @@ function celltab = formatTab_Outputs(cMODEL, cASSET, cESTAT, BENCH)
 % Outputs of all simulations in the set
 
     colHead = {'Country', 'Scenario Run', 'Run Date', 'Asset', ...
-               'Output Metric', 'Period', 'Period Type', 'Net Revenues', ...
+               'Output Metric', 'Period', 'Period Type', 'Branded Net Revenues', ...
                'Branded Point Share', 'Branded Patient Share', 'Branded Units', ...
-               'PTRS %'};
+               'Molecule Point Share', 'Molecule Patient Share', 'PTRS %'};
               
     oStats = {'Mean', 'StdErr', 'Pct01', 'Pct05', 'Pct10', 'Pct15', 'Pct20', ...
               'Pct25', 'Pct30', 'Pct35', 'Pct40', 'Pct45', 'Pct50', 'Pct55', ...
@@ -36,13 +36,15 @@ function celltab = formatTab_Outputs(cMODEL, cASSET, cESTAT, BENCH)
         dateGrid = ESTAT.DateGrid;
                 
         for q = 1:length(oStats)
-            monthlyShareMx = ESTAT.Branded.(oStats{q});  
-            OUT = computeOutputs(MODEL, ASSET, dateGrid, monthlyShareMx);
-            ixYear = find(OUT.Y.YearVec >= 2014 & OUT.Y.YearVec <= 2040);  % Ignore years out of this range
+            monthlyShareMxB = ESTAT.Branded.(oStats{q});
+            monthlyShareMxM = ESTAT.Branded.(oStats{q});
+            OUTB = computeOutputs(MODEL, ASSET, dateGrid, monthlyShareMxB);
+            OUTM = computeOutputs(MODEL, ASSET, dateGrid, monthlyShareMxM);
+            ixYear = find(OUTB.Y.YearVec >= 2014 & OUTB.Y.YearVec <= 2040);  % Ignore years out of this range
             
             for n = 1:length(ASSET.Assets_Rated)
                 
-                if ismember(oStats{q}, oStatsBrief)  % only write brief stats for each year
+                if ismember(oStats{q}, oStatsBrief)  % For yearly data, write brief stats only
                     for p = 1:length(ixYear)
                         rr = rr + 1;
                         celltab{rr, 1} = MODEL.CountrySelected;
@@ -50,45 +52,62 @@ function celltab = formatTab_Outputs(cMODEL, cASSET, cESTAT, BENCH)
                         celltab{rr, 3} = runTime;
                         celltab{rr, 4} = ASSET.Assets_Rated{n};
                         celltab{rr, 5} = oStats{q};
-                        celltab{rr, 6} = OUT.Y.YearVec(ixYear(p));
+                        celltab{rr, 6} = OUTB.Y.YearVec(ixYear(p));
                         celltab{rr, 7} = 'Year';
-                        celltab{rr, 8} = OUT.Y.NetRevenues(n, ixYear(p));
-                        celltab{rr, 9} = OUT.Y.PointShare(n, ixYear(p));
-                        celltab{rr, 10} = OUT.Y.PatientShare(n, ixYear(p));
-                        celltab{rr, 11} = OUT.Y.Units(n, ixYear(p));
-                        celltab{rr, 12} = ASSET.Scenario_PTRS{n};
+                        celltab{rr, 8} = OUTB.Y.NetRevenues(n, ixYear(p));
+                        celltab{rr, 9} = OUTB.Y.PointShare(n, ixYear(p));
+                        celltab{rr, 10} = OUTB.Y.PatientShare(n, ixYear(p));
+                        celltab{rr, 11} = OUTB.Y.Units(n, ixYear(p));
+                        celltab{rr, 12} = OUTM.Y.PointShare(n, ixYear(p));
+                        celltab{rr, 13} = OUTM.Y.PatientShare(n, ixYear(p));
+                        celltab{rr, 14} = ASSET.Scenario_PTRS{n};
                     end
                 end
                 
                 % Cume and Peak values for years up to and including the year after LOE
-                rr = rr + 1;
-                celltab(rr,:) = celltab(rr-1,:);
-                celltab{rr,6} = '';  % no text in a numeric "Year" column
-                celltab{rr,7} = 'Peak';
-                ix = (OUT.Y.YearVec >= 2014) & (OUT.Y.YearVec <= (ASSET.LOE_Year{n} + 1));
-                if sum(ix) > 0
-                    peak8 = nanmax(OUT.Y.NetRevenues(n, ix));
-                    peak9 = nanmax(OUT.Y.PointShare(n, ix));
-                    peak10 = nanmax(OUT.Y.PatientShare(n, ix));
-                    peak11 = nanmax(OUT.Y.Units(n, ix));
+                ix = (OUTB.Y.YearVec >= 2014) & (OUTB.Y.YearVec <= (ASSET.LOE_Year{n} + 1));
 
-                    celltab(rr, 8:11) = {peak8, peak9, peak10, peak11};
-                else
-                    celltab(rr, 8:11) = {0, 0, 0, 0};                    
-                end                
-                
+                % Peak Values -----------------------------
                 rr = rr + 1;
-                celltab(rr,:) = celltab(rr-1,:);
-                celltab{rr,6} = '';  % no text in a numeric "Year" column               
-                celltab{rr,7} = 'Cume';
+                celltab{rr, 1} = MODEL.CountrySelected;
+                celltab{rr, 2} = MODEL.ScenarioSelected;
+                celltab{rr, 3} = runTime;
+                celltab{rr, 4} = ASSET.Assets_Rated{n};
+                celltab{rr, 5} = oStats{q};
+                celltab{rr, 6} = '';  % no text in a numeric "Year" column
+                celltab{rr, 7} = 'Peak';
                 if sum(ix) > 0
-                    cume8  = nansum(OUT.Y.NetRevenues(n, ix));
-                    cume11 = nansum(OUT.Y.Units(n, ix));
-
-                    celltab(rr, 8:11) = {cume8, nan, nan, cume11};
+                    peak8 = nanmax(OUTB.Y.NetRevenues(n, ix));
+                    peak9 = nanmax(OUTB.Y.PointShare(n, ix));
+                    peak10 = nanmax(OUTB.Y.PatientShare(n, ix));
+                    peak11 = nanmax(OUTB.Y.Units(n, ix));
+                    peak12 = nanmax(OUTM.Y.PointShare(n, ix));
+                    peak13 = nanmax(OUTM.Y.PatientShare(n, ix));
+                    
+                    celltab(rr, 8:13) = {peak8, peak9, peak10, peak11, peak12, peak13};
                 else
-                    celltab(rr, 8:11) = {0, nan, nan, 0};               
+                    celltab(rr, 8:13) = {0, 0, 0, 0, 0, 0};                    
                 end
+                celltab{rr, 14} = ASSET.Scenario_PTRS{n};
+                
+                % Cume Values -----------------------------
+                rr = rr + 1;
+                celltab{rr, 1} = MODEL.CountrySelected;
+                celltab{rr, 2} = MODEL.ScenarioSelected;
+                celltab{rr, 3} = runTime;
+                celltab{rr, 4} = ASSET.Assets_Rated{n};
+                celltab{rr, 5} = oStats{q};
+                celltab{rr, 6} = '';  % no text in a numeric "Year" column               
+                celltab{rr, 7} = 'Cume';
+                if sum(ix) > 0
+                    cume8  = nansum(OUTB.Y.NetRevenues(n, ix));
+                    cume11 = nansum(OUTB.Y.Units(n, ix));
+
+                    celltab(rr, 8:13) = {cume8, nan, nan, cume11, nan, nan};
+                else
+                    celltab(rr, 8:13) = {0, nan, nan, 0, nan, nan};               
+                end
+                celltab{rr, 14} = ASSET.Scenario_PTRS{n};
                 
             end
             
