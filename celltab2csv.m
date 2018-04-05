706e6444839624f15856d286828193d79fc6dd34
patrickshami <patrickshami@e7fa7c23-505d-4fac-8005-645d41c176d8>
2018-04-05 23:23:17 +0000

    Include two new files omitted in the last commit

---
+++ celltab2csv.m
@@ -0,0 +1,31 @@
+function celltab2csv(filename, celltab, fmt)
+
+    [Nr, Nc] = size(celltab);
+
+    fmt1 = [repmat('%s,', [1, Nc-1]), '%s\n'];
+    if nargin < 3 
+        fmt = fmt1;
+    end
+    
+    cFmt = strsplit(fmt, ',');
+    for m = 1:length(cFmt)
+        if ~isempty(strfind(cFmt{m}, 'f'))  % Remove NaN values so they write as empty
+            ix = cellisnan(celltab(:, m));
+            celltab(ix,m) = cell(sum(ix),1);
+        elseif ~isempty(strfind(cFmt{m}, 's'))  % Remove commas so they don't break the format
+            ix = cellfun(@ischar,celltab(:,m));
+            celltab(ix,m) = strrep(celltab(ix,m), ',', '');           
+        end
+    end
+
+
+    fid = fopen(filename, 'w');
+    fprintf(fid, fmt1, celltab{1,:});  % write header row
+    for m = 2:Nr-1
+        fprintf(fid, fmt, celltab{m,:});
+    end
+    fclose(fid);
+
+
+
+end
